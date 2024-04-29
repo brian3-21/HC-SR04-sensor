54ad600b3f4d5c76e06b27ce7e3fef66423e3bd2
Brian3-21 <brianperezdota@gmail.com>
2024-04-29 17:32:26 -0400

    add buzzer

--- hc.ino
+++ hc.ino
@@ -1,14 +1,23 @@
 int Trig = 10;
 int Eco  = 9;
 int Led  = 8;
+int Buzz = 7;
 
 int duracion;
 int distancia;
+
+
+void buzzSong (int dela){
+  digitalWrite(Buzz, HIGH);
+  delay(dela);
+  digitalWrite(Buzz, LOW);
+}
 
 void setup() {
   pinMode(Trig,OUTPUT);
   pinMode(Eco,INPUT);
   pinMode(Led, OUTPUT);
+  pinMode(Buzz, OUTPUT);
   Serial.begin(9600);
   // put your setup code here, to run once:
 
@@ -20,13 +29,20 @@ void loop() {
   digitalWrite(Trig, LOW);
   duracion = pulseIn(Eco, HIGH);
   distancia = duracion / 58.2;
-  Serial.println(distancia);
+  Serial.println(distancia );
   delay(200);
 
   if(distancia < 10){
     digitalWrite(Led, HIGH);
+    buzzSong(50);
   } else {
     digitalWrite(Led, LOW);
+    
+  }
+
+
+  if(distancia < 40){
+    buzzSong(400);
   }
 
 
